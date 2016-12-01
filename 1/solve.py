29c29aeb80bbd5aa1ad2d8628e1d1965775183d6
Michael Paulauski <mpaulauski@nopsec.com>
2016-12-01 17:51:54 -0500

    almost there

--- 1/solve.py
+++ 1/solve.py
@@ -1,4 +1,23 @@
 file = open("input.txt")
 
+current_direction = 0
+x = 0
+y = 0
+
 for line in file:
-	print line.split()
+    for command in line.split():
+        direction = command[0]
+        distance = int(command[1])
+        new_dir = 90 if direction == "R" else -90
+        current_direction = (current_direction + new_dir) % 360
+        if current_direction == 0:
+            y += distance
+        elif current_direction == 90:
+            x += distance
+        elif current_direction == 180:
+            y -= distance
+        else:
+            x -= distance
+
+    dist = abs(x) + abs(y)
+    print dist
